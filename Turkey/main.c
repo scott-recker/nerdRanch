d929b4574b84d1665fc05d61cd22b2a039565339
Scott Recker <scott.recker@banno.com>
2014-11-17 20:10:39 -0600

    Initial Commit

---
+++ Turkey/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  Turkey
+//
+//  Created by Scott on 11/17/14.
+//  Copyright (c) 2014 Big Nerd Ranch. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
